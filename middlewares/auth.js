aeb7a4a6b4a37b396475f7f9034a1cea9448e350
Ishan Manandhar <ishan02016@gmail.com>
2020-08-22 18:01:48 +0545

    working code with some fixes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,13 @@
-// Middleware for auth route
+// Middleware for auth
 
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(" ")[1];
+        const token = req.header('x-auth-token');
         jwt.verify(token, "longer-secret-is-better");
         next();
     } catch (error) {
-        res.status(401).json({ message: "No token, denied authorization" });
+        res.status(401).json({ message: "No token cannot authorize" });
     }
 };
